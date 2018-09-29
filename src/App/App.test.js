9b0d758affde747b0c731cb1d845a358a93da8e0
Andrew James <andrewpjames87@gmail.com>
2018-09-29 14:07:39 -0600

    Add jest and enzyme

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
 import App from './App';
 
+function setup() {
+  const props = {};
+  const comp = shallow(<App />);
+  return { comp, props };
+}
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const { comp } = setup();
+  expect(comp.find('.hello').length).toBe(1);
 });
