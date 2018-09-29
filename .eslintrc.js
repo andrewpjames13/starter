892f37a42344fab1ead2f8424144026c4c4fe6e5
Andrew James <andrewpjames87@gmail.com>
2018-09-29 14:36:31 -0600

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports =  {
+  extends: "airbnb",
+  parser: "babel-eslint",
+  plugins: [
+    "import"
+  ],
+  settings: {
+    'import/resolver': {
+      'webpack': { config: 'config/webpack.config.dev.js' }
+    }
+  },
+  env: {
+    jest: true
+  },
+  globals: {
+    "document": false,
+    "window": true
+  },
+  rules: {
+    "import/no-named-as-default": [0],
+    "import/no-extraneous-dependencies": [0, { "devDependencies": true }],
+    "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
+    "react/jsx-tag-spacing": [1],
+    "jsx-a11y/label-has-for": [1],
+    "react/jsx-boolean-value": [0],
+    "react/no-array-index-key": [0]
+  }
+}
