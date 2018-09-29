a551eface1fbe3a79fbc86182766a085bdedc4c0
Andrew James <andrewpjames87@gmail.com>
2018-09-29 16:31:20 -0600

    Add redux to project with dev tool

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement,
+);
+
 registerServiceWorker();
